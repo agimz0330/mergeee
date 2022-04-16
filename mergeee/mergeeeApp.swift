bc51f2a42a3efd346e1b3ab078cd5c3de78f588e
BananaChiu <agimz0330@gmail.com>
2022-04-17 03:49:17 +0800

    Initial Commit

---
+++ mergeee/mergeeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mergeeeApp.swift
+//  mergeee
+//
+//  Created by User23 on 2022/4/17.
+//
+
+import SwiftUI
+
+@main
+struct mergeeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
